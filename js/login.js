612745b54037d24ca2c5f7b2706b873d16cef1b7
fdominguez <68867161+facundodominguez@users.noreply.github.com>
2020-10-02 10:10:49 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,2 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    document.getElementById("submitButton").addEventListener("click", function () {
-
-        var email = document.getElementById("inputEmail");
-        localStorage.setItem("inputEmail", email.value);
-        
-    });
 });
