4d311d1269b0f77ad775eb098a3acca154a057c8
fdominguez <68867161+facundodominguez@users.noreply.github.com>
2020-10-02 10:07:07 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,16 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-
-    const backUrl = 'http://localhost:8080';
-    const loginForm = document.getElementById('loginForm');
-    loginForm.onsubmit = (e) => {
-        e.preventDefault()
-        const user = document.getElementById('inputEmail').value;
-        const password = document.getElementById('inputPassword').value;
-        checkUser('/auth', {
-            user,
-            password
-        });
-    };
     document.getElementById("submitButton").addEventListener("click", function () {
 
         var email = document.getElementById("inputEmail");
